35629a509fbd12f2dcaa5fecef41f904ba3f81e7
leonard <leonardrolandvictor@googlemail.com>
2022-12-04 18:23:40 +0100

    Deploy V2

--- .idea/src/App/UserManager.java
+++ .idea/src/App/UserManager.java
@@ -16,16 +16,19 @@ public class UserManager {
         String Choice = null;
         Account UserAccount = null;
 
-        while (exit == false){
+        while (exit == false) {
             System.out.println("Willkommen bei RegistrationManager!");
             System.out.println("Wählen sie eine der folgenden Optionen aus: 1:Registrieren,2:Einloggen,3:Schliessen");
             Choice = RegistrationManager.TerminalReader();
 
 
-                if(Choice=="1") {
+            switch (Choice) {
+
+
+                case "1":
                     RegistrationManager.register();
-                }
-                if(Choice=="2") {
+                    break;
+                case "2":
                     UserAccount = LoginService.requestUserCredentials();
 
                     if (UserAccount == null) {
@@ -33,36 +36,42 @@ public class UserManager {
                     } else {
                         loggedin = true;
                     }
-                }
-                if(Choice=="3") {
+                    break;
+
+                case "3":
                     System.out.println("Auf wiedersehen");
                     exit = true;
-                }
+                    break;
 
-                else{
+                default:
                     System.out.println("Dies ist keine gültige Option, bitte nochmal versuchen");
-                }
+            }
+
 
             while(loggedin){
                 System.out.println("Sie wurden eingeloggt");
                 System.out.println("Wählen sie eine der folgenden Optionen aus: 1:Passwort aendern,2:Konto löschen,3:Ausloggen");
                 Choice = RegistrationManager.TerminalReader();
 
-               if(Choice=="1") {
-                       PasswordChange.changePassword(UserAccount);
-               }
-                if(Choice=="2") {
+                switch(Choice) {
+
+                    case "1":
+                        PasswordChange.changePassword(UserAccount);
+                        break;
+
+                    case "2":
                         RegistrationManager.DeleteAccount(UserAccount);
                         loggedin = false;
-                }
-                if(Choice=="3") {
+                        break;
+
+                    case "3":
                         loggedin = false;
                         System.out.println("Sie wurden ausgeloggt");
-                }
-                else{
+                        break;
+
+                    default:
                         System.out.println("Dies ist keine gültige Option, bitte nochmal versuchen");
                 }
-
 
             }
         }
