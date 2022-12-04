bfb15af482d1874c3cca998bfa3afc170f173ccc
turgut aydemir <turgut.aydemir@stud.fh-campuswien.ac.at>
2022-12-04 17:15:05 +0100

    added changePassword method

--- .idea/src/App/PasswordChange.java
+++ .idea/src/App/PasswordChange.java
@@ -1,5 +1,8 @@
 package App;
 
+import App.JsonLoader;
+import App.RegistrationManager;
+
 public class PasswordChange {
     private String initiallyEnteredPass;
     private String secondlyEnteredPass;
@@ -20,5 +23,23 @@ public class PasswordChange {
         this.secondlyEnteredPass = secondlyEnteredPass;
     }
 
-    public void changePassword(){}//
+    public static boolean confirmEnteredPasswordsSame(String initiallyEnteredPass, String secondlyEnteredPass){
+        return secondlyEnteredPass.equals(initiallyEnteredPass);
+    }
+
+    public void changePassword(Account account){
+        System.out.println("Please enter your old password");
+        String oldpasswordHashed = RegistrationManager.PasswordHasher(RegistrationManager.TerminalReader());
+        if (oldpasswordHashed.equals(account.getPasswort())){
+            System.out.println("Please enter your new password");
+            initiallyEnteredPass = RegistrationManager.TerminalReader();
+            System.out.println("Please re-enter your new password");
+            secondlyEnteredPass = RegistrationManager.TerminalReader();
+            if (confirmEnteredPasswordsSame(initiallyEnteredPass,secondlyEnteredPass)){
+                account.setPasswort(RegistrationManager.PasswordHasher(secondlyEnteredPass));
+                JsonLoader.SaveJson(account);
+                System.out.println("Password has been changed succesfully");
+        }}
+
+    }
 }
