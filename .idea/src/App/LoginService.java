101c7c800653aef57f28094c4ebfb8173adbf544
Timur <timur.idalov@stud.fh-campuswien.ac.at>
2022-12-04 22:34:44 +0100

    add instance variable to account add functionality to LoginService

--- .idea/src/App/LoginService.java
+++ .idea/src/App/LoginService.java
@@ -5,23 +5,52 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import App.*;
+//import java.util.concurrent.TimeUnit;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class LoginService {
 
-    public static boolean areLoginAndPasswordCorrect(String username, String password) {
+    public static boolean jsonExist(String username) {
         File f = new File(".idea/src/App/Database/"+username+".json");
-        if (f.exists() && !f.isDirectory()) {
+        return f.exists() && !f.isDirectory();
+    }
+
+    public static Account getAccountFromUsername(String username) {
+        if (jsonExist(username)) {
             Account account = new Account();
             account = JsonLoader.ReadFromJson(username);
-            if (account.getPasswort().equals(RegistrationManager.PasswordHasher(password))) {
+            return account;
+        }
+        return null;
+    }
+
+    public static boolean areLoginAndPasswordCorrect(String username, String password) {
+        Account account = new Account();
+        account = getAccountFromUsername(username);
+        if (account.getPasswort().equals(RegistrationManager.PasswordHasher(password))) {
                 return true;
-            }
         }
         return false;
     }
 
+    public static boolean isUserLocked(String username) {
+        Account account = new Account();
+        account = getAccountFromUsername(username);
+        if (account.getIsLocked()) {
+                return true;
+        }
+        return false;
+    }
+
+    public static String getCurrentDateTimeString() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String currentDateTimeString = dtf.format(currentDateTime).toString();
+        return currentDateTimeString;
+    }
+
     public static Account requestUserCredentials() {
-        short failedLoginAttempts = 0;
         boolean userLocked = false;
         boolean correctpassword = false;
         Scanner keyboard = new Scanner(System.in);
@@ -30,57 +59,47 @@ public class LoginService {
         System.out.println("Bitte gib einen Passwort ein:");
         String userInputPassword = keyboard.next();
         if (areLoginAndPasswordCorrect(userInputUsername, userInputPassword)) {
-            System.out.println("Du bist erfolgreich eingeloggt.");
             correctpassword = true;
             Account account = new Account();
             account = JsonLoader.ReadFromJson(userInputUsername);
+            if (account.getIsLocked()) {
+                System.out.println("Dein Benutzer ist bis zum " + account.getDateTimeTillUserWasLocked() + " gesperrt.");
+                // TODO: check the time and unlock user
+                account.setIsLocked(false);
+                JsonLoader.SaveJson(account);
+                return null;
+            }
+            System.out.println("Du bist erfolgreich eingeloggt.");
+            account.setNumberOfFailedLogins(0);
+            JsonLoader.SaveJson(account);
             return account;
         } else {
             System.out.println("Benutzername oder Passwort ist falsch.");
+            if (jsonExist(userInputUsername)) {
+                Account account = new Account();
+                account = JsonLoader.ReadFromJson(userInputUsername);
+                int numberOfFailedLogins = 0;
+                numberOfFailedLogins = account.getNumberOfFailedLogins() + 1;
+                account.setNumberOfFailedLogins(numberOfFailedLogins);
+                if (numberOfFailedLogins >= 4) {
+                    account.setIsLocked(true);
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+                    LocalDateTime dateTimeWhenUserWasLocked = LocalDateTime.now();
+                    LocalDateTime dateTimeTillUserWasLocked = dateTimeWhenUserWasLocked.plusSeconds(60);
+
+                    String dateTimeWhenUserWasLockedString = dtf.format(dateTimeWhenUserWasLocked).toString();
+                    String dateTimeTillUserWasLockedString = dtf.format(dateTimeTillUserWasLocked).toString();
+
+                    account.setDateTimeWhenUserWasLocked(dateTimeWhenUserWasLockedString);
+                    account.setDateTimeTillUserWasLocked(dateTimeTillUserWasLockedString);
+
+                    System.out.println("Dein Account ist gesperrt.");
+                }
+                JsonLoader.SaveJson(account);
+
+
+            }
             return null;
         }
     }
-/*
-    static String inputPath = ".idea/src/App/Database/";
-
-    public static void getUsernameInputAndCheckIfExists() {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter your username:");
-        String userInputUsername = keyboard.next();
-        File tmpFileObj = new File(inputPath + userInputUsername + ".json");
-        if (tmpFileObj.exists()) {
-            getPasswordInputAndCheckIfValid(userInputUsername);
-        } else {
-            System.out.println("No user registered with this username.");
-            getUsernameInputAndCheckIfExists();
-        }
-        keyboard.close();
-    }
-
-    public static void getPasswordInputAndCheckIfValid(String username) {
-        short numberOfFailedLogins = 0;
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter your password:");
-        String userInputPassword = keyboard.next();
-        if (isPasswordCorrect(username, userInputPassword)) {
-            System.out.println("You are successfully logged in.");
-        } else {
-            numberOfFailedLogins = +1;
-            System.out.println("Benutzername oder Passwort nicht korrekt.");
-            if (numberOfFailedLogins != 3) {
-                getPasswordInputAndCheckIfValid(username);
-            } else {
-                // TODO: implement countdown
-                return;
-            }
-        }
-    }
-
-    public static boolean isPasswordCorrect(String username, String password) {
-        Account account = new Account();
-        account = JsonLoader.ReadFromJson(username);
-        return account.getPasswort().equals(password);
-    }
-
- */
 }
