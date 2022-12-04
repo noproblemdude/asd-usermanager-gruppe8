63de1bc5654de706579c01e6be0a1b43f3b0d380
leonard <leonardrolandvictor@googlemail.com>
2022-12-04 11:29:45 +0100

    Relative Path added to JsonLoader and registrationmanager added

--- .idea/src/App/JsonLoader.java
+++ .idea/src/App/JsonLoader.java
@@ -10,12 +10,8 @@ import java.util.Scanner; // Import the Scanner class to read text files
 import App.common.*;
 
 public class JsonLoader{
-    public static void main(String args[]) throws JsonGenerationException, JsonMappingException, IOException {
-        Account Acc1 = new Account();
-        Acc1.setBenutzername("Musterman");
-        SaveJson(Acc1);
-        Account Acc2 = ReadFromJson("Musterman");
-    }
+
+    public static void main(String args[]){}
 
     public static Account FromJson(String JsonString) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -38,13 +34,10 @@ public class JsonLoader{
 
     public static void SaveJson(Account Acc){
 
-
-        File myObj = new File("C:\\Users\\leona\\IdeaProjects\\asd-usermanager-gruppe8\\.idea\\src\\App\\Database\\"+Acc.getBenutzername()+".json");
-
         String JsonString = ToJson(Acc);
 
         try {
-            FileWriter myWriter = new FileWriter("C:\\Users\\leona\\IdeaProjects\\asd-usermanager-gruppe8\\.idea\\src\\App\\Database\\"+Acc.getBenutzername()+".json");
+            FileWriter myWriter = new FileWriter(".idea/src/App/Database/"+Acc.getBenutzername()+".json");
             myWriter.write(JsonString);
             myWriter.close();
         } catch (IOException e) {
@@ -52,12 +45,13 @@ public class JsonLoader{
             e.printStackTrace();
         }
 
+
     }
 
     public static Account ReadFromJson(String AccName) {
         String jsonString = null;
         try {
-            File myObj = new File("C:\\Users\\leona\\IdeaProjects\\asd-usermanager-gruppe8\\.idea\\src\\App\\Database\\" + AccName + ".json");
+            File myObj = new File(".idea/src/App/Database/"+AccName+".json");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 jsonString = myReader.nextLine();
---
+++ .idea/src/App/RegistrationManager.java
@@ -0,0 +1,101 @@
+package App;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import App.common.*;
+import java.security.MessageDigest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class RegistrationManager {
+
+    public static void main(String args[]){
+        register();
+    }
+
+    public static void register(){
+        String Vorname = null;
+        String Nachname = null;
+        String Benutzername = null;
+        String Passwort = null;
+
+        //Creating a File object for directory
+        File directoryPath = new File(".idea/src/App/Database/");
+        //List of all files and directories
+        String contents[] = directoryPath.list();
+
+        List<String> Accounts =Arrays.asList(contents);
+
+        System.out.println("Bitte gib einen Benutzernamen ein");
+        Benutzername = TerminalReader();
+
+        if(Accounts.contains(Benutzername+".json")){
+            System.out.println("Benutzername ist schon vergeben");
+        }
+        else {
+            Account Acc = new Account();
+
+            System.out.println("Bitte gib deinen Vornamen ein");
+            Vorname = TerminalReader();
+            Acc.setVorname(Vorname);
+            System.out.println("Bitte gib deinen Nachnamen ein");
+            Nachname = TerminalReader();
+            Acc.setNachname(Nachname);
+            Acc.setBenutzername(Benutzername);
+            System.out.println("Bitte gib einen Passwort ein");
+            Passwort = TerminalReader();
+            Acc.setPasswort(PasswordHasher(Passwort));
+
+            JsonLoader.SaveJson(Acc);
+
+            System.out.println("Account wurde angelegt!");
+
+        }
+
+    }
+
+    public static String PasswordHasher(String Password){
+        StringBuilder sb = null;
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+            messageDigest.update(Password.getBytes());
+
+            byte[] resultByteArray = messageDigest.digest();
+
+            sb = new StringBuilder();
+
+            for (byte b : resultByteArray) {
+                sb.append(String.format("%02x", b));
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
+
+    public static String TerminalReader() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String Input;
+
+        try {
+            Input = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Input;
+    }
+
+
+}
