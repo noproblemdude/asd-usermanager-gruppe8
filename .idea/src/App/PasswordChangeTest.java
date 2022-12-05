67a6915251222aa3942f9a9b463c5747ce28f050
turgut aydemir <turgut.aydemir@stud.fh-campuswien.ac.at>
2022-12-05 18:48:41 +0100

    added Junit library or Spring idk

---
+++ .idea/src/App/PasswordChangeTest.java
@@ -0,0 +1,30 @@
+package App;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordChangeTest {
+
+    @org.junit.jupiter.api.Test
+    void getInitiallyEnteredPass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setInitiallyEnteredPass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSecondlyEnteredPass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSecondlyEnteredPass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void confirmEnteredPasswordsSame() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changePassword() {
+    }
+}
