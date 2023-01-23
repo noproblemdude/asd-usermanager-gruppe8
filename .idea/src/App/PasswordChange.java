d34a51dc3ea464b2b3340f0038bb2e378d61f62d
leonard <leonardrolandvictor@googlemail.com>
2023-01-23 10:13:10 +0100

    ergänzung zum letzten commit (Passwordchange hat gefehlt)

--- .idea/src/App/PasswordChange.java
+++ .idea/src/App/PasswordChange.java
@@ -27,13 +27,13 @@ public class PasswordChange {
     public static void changePassword(Account account){
         System.out.println("Please enter your old password");
         String oldpasswordHashed = RegistrationManager.PasswordHasher(RegistrationManager.TerminalReader());
-        if (oldpasswordHashed.equals(account.getPasswort())){
+        if (oldpasswordHashed.equals(account.getPassword())){
             System.out.println("Please enter your new password");
             String pass1 = RegistrationManager.TerminalReader();
             System.out.println("Please re-enter your new password");
             String pass2 = RegistrationManager.TerminalReader();
             if (confirmEnteredPasswordsSame(pass1,pass2)){
-                account.setPasswort(RegistrationManager.PasswordHasher(pass2));
+                account.setPassword(RegistrationManager.PasswordHasher(pass2));
                 JsonLoader.SaveJson(account);
                 System.out.println("Password has been changed succesfully");
         }}
